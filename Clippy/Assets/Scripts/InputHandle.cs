ccff3f11c21aab116fcf77a085f2db3dfdda9b6d
SleepTimeLord <johnp.baldonadoo@gmail.com>
2024-04-28 19:23:46 -0400

    template for talking done

--- Clippy/Assets/Scripts/InputHandle.cs
+++ Clippy/Assets/Scripts/InputHandle.cs
@@ -1,17 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class InputHandle : MonoBehaviour
 {
-    // Start is called before the first frame update
     private Camera _mainCamera;
-
+    private SpriteRenderer sr;
+    public GameObject canvas;
+    private bool isCanvasVisible = false;
 
     private void Awake()
     {
         _mainCamera = Camera.main;
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1) && isCanvasVisible)
+        {
+            HideCanvas();
+        }
     }
 
     public void OnClick(InputAction.CallbackContext context)
@@ -21,6 +29,25 @@ public class InputHandle : MonoBehaviour
         var rayHit = Physics2D.GetRayIntersection(_mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue()));
         if (!rayHit.collider) return;
 
-        Debug.Log(rayHit.collider.gameObject.name);
+        if (rayHit.collider.CompareTag("test1"))
+        {
+            ToggleCanvas();
+        }
+        else if (isCanvasVisible)
+        {
+            HideCanvas();
+        }
+    }
+
+    private void ToggleCanvas()
+    {
+        isCanvasVisible = !isCanvasVisible;
+        canvas.SetActive(isCanvasVisible);
+    }
+
+    private void HideCanvas()
+    {
+        isCanvasVisible = false;
+        canvas.SetActive(false);
     }
 }
